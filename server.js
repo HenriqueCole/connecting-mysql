53fd231358dc0b4f5ea85b2ea1d95017ac9f4f23
HenriqueCole <henriquecolefernandes@gmail.com>
2022-06-22 21:56:19 -0300

    creating an user

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const server = express();
 const cors = require('cors');
 
-const { Sequelize } = require('sequelize');
+const { Sequelize, DataTypes } = require('sequelize');
 const sequelize = new Sequelize('connection', 'root', 'root', {
   host: 'localhost',
   dialect: 'mysql'
@@ -22,8 +22,40 @@ server.get('/', (req, res) => {
   res.json('Hello World');
 });
 
+const User = sequelize.define('User', {
+  firstName: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  lastName: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+},
+  {
+    createdAt: false,
+    updatedAt: false
+  }
+);
+
+server.post('/users', (req, res) => {
+  const { firstName, lastName } = req.body;
+
+  User.create({
+    firstName,
+    lastName
+  }).then(user => {
+    res.json(user);
+  }
+  ).catch(err => {
+    res.json(err);
+  }
+  );
+});
 
 
-server.listen(3000, () => {
+server.listen(3000, async () => {
+  await sequelize.sync();
   console.log('Server is running on port 3000');
 });
+
